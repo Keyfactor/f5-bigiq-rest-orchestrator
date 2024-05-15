4a90687242630bc2d68ab9c90b99cb5ec4ae3da2
leefine02 <kfadmin@LFINE-KF10-ORCH>
2024-05-15 17:43:16 +0000

    ab#58252

--- F5BigIQ/Inventory.cs
+++ F5BigIQ/Inventory.cs
@@ -52,8 +52,7 @@ namespace Keyfactor.Extensions.Orchestrator.F5BigIQ
                 List<F5CertificateItem> certItems =  f5Client.GetCertificates();
                 foreach (F5CertificateItem certItem in certItems)
                 {
-                    logger.LogDebug($"{certItems.IndexOf(certItem).ToString()}");
-                    logger.LogDebug($"{certItem.Alias}");
+                    logger.LogDebug($"Retrieving Alias {certItem.Alias}, item {certItems.IndexOf(certItem).ToString()} of {certItems.Count.ToString()}");
                     if (certItem.FileReference == null)
                         continue;
                     X509Certificate2Collection certChain = f5Client.GetCertificateByLink(certItem.FileReference.Link);
